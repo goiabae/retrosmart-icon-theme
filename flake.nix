113e6d916fd9fa1900fd816374fd98a11b7832dd
goiabae <gm4ch4d0@gmail.com>
2024-11-15 17:53:58 -0300

    update icon theme cache

--- flake.nix
+++ flake.nix
@@ -1,10 +1,26 @@
 {
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.retrosmart-icon-theme =
-      with nixpkgs.legacyPackages.x86_64-linux; pkgs.stdenv.mkDerivation {
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs; in
+      pkgs.stdenv.mkDerivation {
         pname = "retrosmart-icon-theme";
         version = "0.3";
         src = ./.;
+
+        nativeBuildInputs = [ pkgs.gtk3 ];
+
+        propagatedBuildInputs = with pkgs; [
+          gnome-icon-theme hicolor-icon-theme
+        ];
+
+        installPhase = ''
+          runHook preInstall
+          make install
+          gtk-update-icon-cache $out/share/icons/retrosmart-icon-theme
+          runHook postInstall
+        '';
+
+        dontDropIconThemeCache = true;
       };
   };
 }
